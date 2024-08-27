78911d13ef5d81d6f084bc7f48587413efa926e6
eddie ortega <23edortega@gmail.com>
2024-08-27 14:29:46 -0500

    Updated

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -10,40 +10,40 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter your overall score(0-100): ");
-            double grade = double.Parse(Console.ReadLine());
-            char LetterGrade;
+            Console.Write("Enter your overall score(0-100): "); // Askking a user for a score 
+            double grade = double.Parse(Console.ReadLine()); // Reading user input then changing it to a doudble and storing it into variable "grade"
+            char LetterGrade; // Declaring the LetterGrade as char
 
-            if (grade > 89.5)
+            if (grade > 89.5) // if grade is greater than 89.5 then letterGrade is "A"
             {
                 LetterGrade = 'A';
             }
             else
             {
-                if (grade > 79.5)
+                if (grade > 79.5)// if grade is greater than 79.5 then letterGrade is "B"
                 {
                     LetterGrade = 'B';
                 }
                 else
                 {
-                    if (grade > 69.5)
+                    if (grade > 69.5)// if grade is greater than 69.5 then letterGrade is "C"
                     {
                         LetterGrade = 'C';
                     }
                     else
                     {
-                        if (grade > 59.5)
+                        if (grade > 59.5)// if grade is greater than 59.5 then letterGrade is "D"
                         {
                             LetterGrade = 'D';
                         }
                         else
                         {
-                            LetterGrade = 'F';
+                            LetterGrade = 'F';// if grade is less or equal to 59.5 then letterGrade is "F"
                         }
                     }
                 }
             }
-            Console.WriteLine("Your letter grade for score " + grade + " is: " + LetterGrade );
+            Console.WriteLine("Your letter grade for score " + grade + " is: " + LetterGrade);// This Displays the grade and letter into one sentence to let you known score and the lettergrade u get.
             Console.ReadLine();
         }
     }
